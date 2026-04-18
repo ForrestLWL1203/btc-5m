12e45248ee59f5f70e91aad9f2155e5208e526f8
ForrestLiao <forrestliao1203@gmail.com>
2026-04-18 18:06:07 +0800

    feat: update predict package exports for V2

--- polybot/predict/__init__.py
+++ polybot/predict/__init__.py
@@ -1,11 +1,19 @@
 """Auto direction prediction package."""
 
 from .history import WindowHistory, WindowRecord
+from .indicators import ema, rsi, trend_direction, volume_trend
+from .kline import BinanceKlineFetcher, KlineCandle
 from .momentum import DirectionPredictor, MomentumPredictor
 
 __all__ = [
+    "BinanceKlineFetcher",
     "DirectionPredictor",
+    "KlineCandle",
     "MomentumPredictor",
     "WindowHistory",
     "WindowRecord",
+    "ema",
+    "rsi",
+    "trend_direction",
+    "volume_trend",
 ]
