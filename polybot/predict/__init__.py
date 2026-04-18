262929426c075eec403edb0a40c31c58dd33fdc4
ForrestLiao <forrestliao1203@gmail.com>
2026-04-18 16:57:38 +0800

    feat: export predict package public API

--- polybot/predict/__init__.py
+++ polybot/predict/__init__.py
@@ -1,1 +1,11 @@
 """Auto direction prediction package."""
+
+from .history import WindowHistory, WindowRecord
+from .momentum import DirectionPredictor, MomentumPredictor
+
+__all__ = [
+    "DirectionPredictor",
+    "MomentumPredictor",
+    "WindowHistory",
+    "WindowRecord",
+]
